524becf2caf3c574689104ea2025d1ab5d223fb5
sabhyasachisahoo <sabhyasachisahoo16@gmail.com>
2025-07-29 21:21:58 +0530

    created server

--- Intro-to-nodejs-day1/server.js
+++ Intro-to-nodejs-day1/server.js
@@ -1,7 +1,9 @@
 const http = require('http');
 
 
-const server = http.createServer() //server create ho gaya
+const server = http.createServer((req,res)=>{
+    res.end("Hello, World ,Namaste🙏🏽")
+}) //server create ho gaya
 
 server.listen(3000, ()=>{
     console.log("Server is running on port 3000");
