82f17d7074012b2007ea3d1bdc27f04118608bf4
Sabhyasachi Sahoo <sabhyasachisahoo16@gmail.com>
2025-08-19 12:17:16 +0530

    Update db.js

--- Database-day4/src/db/db.js
+++ Database-day4/src/db/db.js
@@ -5,7 +5,7 @@ const mongoose = require('mongoose')
 
 function connectToDB(){
 
-    mongoose.connect("mongodb+srv://query6146:kSXT2QJIbBYiijtR@cluster0.8d5uruk.mongodb.net/cohort")
+    mongoose.connect("YourMongodbURI")
     .then(()=>{
         console.log("Connected to Database");
         
